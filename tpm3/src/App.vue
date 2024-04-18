a9f6c59f377521686329d5dea4e2741f68d24266
Arif Laksonodhewo <arifldhewo234@gmail.com>
2024-04-18 23:06:17 +0700

    Migrating to CLI Version

--- tpm3/src/App.vue
+++ tpm3/src/App.vue
@@ -1,19 +1,41 @@
+<!-- Arif Laksonodhewo -->
+
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1>Todo List</h1>
+    <input v-model="todo" type="text" />
+    <button @click="submitTodoList">Save Me</button>
+    <div>
+      <ol>
+        <li v-for="todo in todoList" v-bind:key="todo">{{ todo }}</li>
+      </ol>
+    </div>
+    <div v-if="isListMoreThanFour">Hebat!</div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+  name: "App",
+  data: function () {
+    return {
+      todo: "",
+      todoList: [],
+    };
+  },
+  methods: {
+    submitTodoList: function () {
+      this.todoList.push(this.todo);
+
+      this.todo = "";
+    },
+  },
+  computed: {
+    isListMoreThanFour: function () {
+      return this.todoList.length >= 4;
+    },
+  },
+};
 </script>
 
 <style>
@@ -21,7 +43,6 @@ export default {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
   color: #2c3e50;
   margin-top: 60px;
 }

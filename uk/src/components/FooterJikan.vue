d985cbc755cee225ed6b8f5749648173bbc5e0da
Arif Laksonodhewo <arifldhewo234@gmail.com>
2024-04-27 20:35:19 +0700

    Final

---
+++ uk/src/components/FooterJikan.vue
@@ -0,0 +1,19 @@
+<template>
+  <div
+    class="w-full mt-96 md:mt-80 xl:mt-72 2xl:mt-[500px] flex justify-evenly items-center font-merriweathers-sans"
+  >
+    <img
+      class="w-[95px]"
+      src="https://jikan.moe/assets/images/logo/jikan.logo.png"
+      alt="jikan-logo"
+    />
+    <img class="w-[95px]" src="@/assets/vue_logo.png" alt="jikan-logo" />
+  </div>
+</template>
+
+<script>
+export default {
+  name: "FooterJikan",
+  components: {},
+};
+</script>
---
+++ uk/src/views/LandingView.vue
@@ -0,0 +1,43 @@
+<template>
+  <div class="home">
+    <SidebarGlob></SidebarGlob>
+    <div class="w-full flex justify-center">
+      <MalLogo></MalLogo>
+    </div>
+    <div class="mx-10 lg:mx-20 xl:mx-32 xl:w-1/2">
+      <p class="text-base md:text-xl lg:text-2xl xl:text-4xl">
+        Using <span class="font-merriweathers-sans">Jikan</span> REST API v4 for
+        showing anime list from the biggest aggregator anime list
+      </p>
+      <ButtonPrimary
+        @click="goToHome"
+        class="font-bold text-sm"
+        text="GET STARTED"
+      ></ButtonPrimary>
+    </div>
+    <FooterJikan></FooterJikan>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import SidebarGlob from "@/components/SidebarGlob.vue";
+import MalLogo from "../components/MalLogo.vue";
+import FooterJikan from "@/components/FooterJikan.vue";
+import ButtonPrimary from "@/components/ButtonPrimary.vue";
+
+export default {
+  name: "HomeView",
+  components: {
+    SidebarGlob,
+    MalLogo,
+    FooterJikan,
+    ButtonPrimary,
+  },
+  methods: {
+    goToHome() {
+      this.$router.push("/anilist");
+    },
+  },
+};
+</script>
